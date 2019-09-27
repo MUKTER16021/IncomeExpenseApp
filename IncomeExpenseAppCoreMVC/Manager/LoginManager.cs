0393096fbfa4026ff160cefe1b07ea621c7e2db0
Md Mukter Hossain <mukter87718@gmail.com>
2019-09-27 17:19:59 +0600

    Login Page development code and declare another class

---
+++ IncomeExpenseAppCoreMVC/Manager/LoginManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IncomeExpenseAppCoreMVC.Gateway;
+using IncomeExpenseAppCoreMVC.Models;
+
+namespace IncomeExpenseAppCoreMVC.Manager
+{
+    public class LoginManager
+    {
+        private LoginGateway loginGateway;
+        public LoginManager()
+        {
+            loginGateway=new LoginGateway();
+        }
+
+        public Login LoginInfo(Login login)
+        {
+            Login loginInfo = loginGateway.LoginInfo(login);
+            return loginInfo;
+        }
+    }
+}
--- IncomeExpenseAppCoreMVC/Startup.cs
+++ IncomeExpenseAppCoreMVC/Startup.cs
@@ -58,7 +58,7 @@ namespace IncomeExpenseAppCoreMVC
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Login}/{action=LoginToFrom}/{id?}");
             });
         }
     }
