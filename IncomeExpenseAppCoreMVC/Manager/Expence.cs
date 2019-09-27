a7b99ddb0c172200a5d399256525b8f477e37852
EngShohag <shohagrahmanjb@gmail.com>
2019-09-27 16:58:56 +0600

    ExpenseManager Add

---
+++ IncomeExpenseAppCoreMVC/Manager/Expence.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncomeExpenseAppCoreMVC.Manager
+{
+    public class Expence
+    {
+    }
+}
